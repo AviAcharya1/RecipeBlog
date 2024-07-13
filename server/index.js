69900a76c8b7e54435b05099d77658ebeb69e7b9
aviacharya01 <aviacharyasr@gmail.com>
2024-07-13 16:33:40 +0530

    some Changes

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const router = express.Router();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: ["https://recipe-blog-c06e4c8d6-aviacharya1s-projects.vercel.app/signup"],
   methods: ["POST", "GET", "PUT", "DELETE"],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
